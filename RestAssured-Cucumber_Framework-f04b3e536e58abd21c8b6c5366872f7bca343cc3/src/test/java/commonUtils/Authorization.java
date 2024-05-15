312c6ab755e159b3339c69a7aed1797b31c41c71
GeetlaNarendarreddy <58568170+GeetlaNarendarreddy@users.noreply.github.com>
2024-05-15 20:47:50 +0530

    Token Added

--- RestAssured-Cucumber_Framework-f04b3e536e58abd21c8b6c5366872f7bca343cc3/src/test/java/commonUtils/Authorization.java
+++ RestAssured-Cucumber_Framework-f04b3e536e58abd21c8b6c5366872f7bca343cc3/src/test/java/commonUtils/Authorization.java
@@ -2,7 +2,7 @@ package commonUtils;
 
 public class Authorization {
 
-    private static final String token = "Bearer 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";
+    private static final String token = "Bearer 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";
 
     public static String getToken() {
         return token;
